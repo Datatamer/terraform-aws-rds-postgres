14a6138c3ccea354c9567dc8b5fc57829a6be211
Hugo Barros <hugo.barros@caylent.com>
2021-11-03 17:35:30 -0300

    creates param group log parameters from module variables

--- variables.tf
+++ variables.tf
@@ -27,6 +27,18 @@ variable "parameter_group_name" {
   default     = "rds-postgres-pg"
 }
 
+variable "param_log_min_duration_statement" {
+  description = "(ms) Sets the minimum execution time above which statements will be logged."
+  type        = string
+  default     = "-1"
+}
+
+variable "param_log_statement" {
+  description = "Sets the type of statements logged. Valid values are none, ddl, mod, all"
+  type        = string
+  default     = "none"
+}
+
 variable "identifier_prefix" {
   description = "Identifier prefix for the RDS instance"
   type        = string
